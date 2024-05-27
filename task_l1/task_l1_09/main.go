e3fc21c2dc367f23598f6d03ac62c682fd746250
Leonov Vladislav <vladleonov21032002@gmail.com>
2024-05-27 13:49:51 +0300

    task_09

---
+++ task_l1/task_l1_09/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fCh := make(chan int)
+	sCh := make(chan int)
+
+	var wg sync.WaitGroup
+
+	// goroutine with anonimous function to write values
+	// from slice into first channel
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for _, elem := range arr {
+			fCh <- elem
+		}
+		close(fCh)
+	}()
+
+	// goroutine with anonimous function to double values
+	// from first channel and write them into second one
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for v := range fCh {
+			sCh <- v * 2
+		}
+		close(sCh)
+	}()
+
+	// goroutine with anonimous function to write values
+	// from second channel into stdout
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for v := range sCh {
+			fmt.Println(v)
+		}
+	}()
+	wg.Wait()
+}
