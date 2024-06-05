5f330835d31c4c696351cc09f2a49aa27655c069
Leonov Vladislav <vladleonov21032002@gmail.com>
2024-06-05 16:12:11 +0300

    pattern_03

--- task_l2/pattern/03_visitor.go
+++ task_l2/pattern/03_visitor.go
@@ -1,7 +1,95 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «посетитель».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Visitor_pattern
 */
+
+// Применимость:
+// 1) Когда нужно выполнить операцию над разными типами.
+// 2) Когда нужно выполнить операции над объектами, косвенно связанные с ними.
+
+// Плюсы:
+// 1) Упрощения добавления операций для сложных структурных объектов.
+// 2) Объединение родственных операций в одном классе.
+
+// Минусы:
+// 1) Паттерн не оправдан, если часто меняется иерархия элементов.
+// 2) Приводит к нарушению инкапсуляции элементов.
+
+type Square struct {
+	side int
+}
+
+func (s *Square) accept(v Visitor) {
+	v.visitForSquare(s)
+}
+
+func (s *Square) getType() string {
+	return "Square"
+}
+
+type Circle struct {
+	radius int
+}
+
+func (c *Circle) accept(v Visitor) {
+	v.visitForCircle(c)
+}
+
+func (c *Circle) getType() string {
+	return "Circle"
+}
+
+type Rectangle struct {
+	l int
+	b int
+}
+
+func (t *Rectangle) accept(v Visitor) {
+	v.visitForRectangle(t)
+}
+
+func (t *Rectangle) getType() string {
+	return "rectangle"
+}
+
+type Visitor interface {
+	visitForSquare(*Square)
+	visitForCircle(*Circle)
+	visitForRectangle(*Rectangle)
+}
+
+type AreaCalculator struct {
+	area int
+}
+
+func (a *AreaCalculator) visitForSquare(s *Square) {
+	fmt.Println("Calculating area for square")
+}
+
+func (a *AreaCalculator) visitForCircle(s *Circle) {
+	fmt.Println("Calculating area for circle")
+}
+func (a *AreaCalculator) visitForRectangle(s *Rectangle) {
+	fmt.Println("Calculating area for rectangle")
+}
+
+type MiddleCoordinates struct {
+	x int
+	y int
+}
+
+func (a *MiddleCoordinates) visitForSquare(s *Square) {
+	fmt.Println("Calculating middle point coordinates for square")
+}
+
+func (a *MiddleCoordinates) visitForCircle(c *Circle) {
+	fmt.Println("Calculating middle point coordinates for circle")
+}
+func (a *MiddleCoordinates) visitForRectangle(t *Rectangle) {
+	fmt.Println("Calculating middle point coordinates for rectangle")
+}
