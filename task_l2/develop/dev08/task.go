3daa340a63fa939b90442913f1d5a6ba0dd3a8bd
Leonov Vladislav <vladleonov21032002@gmail.com>
2024-06-21 01:00:04 +0300

    dev_08

--- task_l2/develop/dev08/task.go
+++ task_l2/develop/dev08/task.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"os/user"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/mitchellh/go-ps"
+)
+
 /*
 === Взаимодействие с ОС ===
 
@@ -9,11 +23,176 @@ package main
 поддержать fork/exec команды
 конвеер на пайпах
 
-Реализовать утилиту netcat (nc) клиент
-принимать данные из stdin и отправлять в соединение (tcp/udp)
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
+func RemovePlaceholders(str string, replacements map[int]string) string {
+	re := regexp.MustCompile(`<not-pipe>\d+</not-pipe>`)
+	if re.MatchString(str) {
+		for _, item := range re.FindAllString(str, -1) {
+			key := strings.TrimLeft(item, `<not-pipe>`)
+			key = strings.Trim(key, `</not-pipe>`)
+			keyInt, _ := strconv.Atoi(key)
+			str = strings.Replace(str, item, replacements[keyInt], -1)
+		}
+	}
+	return str
+}
+
+func Parse(command string) string {
+	re := regexp.MustCompile(`\$\((.*?)\)`)
+	submatchall := re.FindAllString(command, -1)
+	if len(submatchall) != 0 {
+		for _, item := range submatchall {
+			element := strings.Trim(item, `$(`)
+			element = strings.Trim(element, `)`)
+			result := Parse(element)
+			command = strings.Replace(command, item, result, -1)
+		}
+	}
+	replacements := map[int]string{}
+	re = regexp.MustCompile(`"(.*?)\|(.*?)"`)
+	submatchall = re.FindAllString(command, -1)
+	if len(submatchall) != 0 {
+		for i, item := range submatchall {
+			replacements[i] = item
+			command = strings.Replace(command, item, fmt.Sprintf("<not-pipe>%v</not-pipe>", i), -1)
+		}
+	}
+	output := ""
+
+	if strings.Contains(command, `|`) {
+		pipeArgs := ``
+		for _, pipe := range strings.Split(command, `|`) {
+			pipe = RemovePlaceholders(pipe, replacements)
+			pipe = strings.TrimSpace(pipe)
+			currentCommand := strings.Split(pipe, ` `)
+			if strings.Contains(currentCommand[0], `pwd`) {
+				pipeArgs, _ = os.Getwd()
+			} else if strings.Contains(currentCommand[0], `echo`) {
+				pipeArgs = strings.Join(currentCommand[1:], ` `)
+				output = pipeArgs
+			} else if strings.Contains(currentCommand[0], `ps`) {
+				pipeArgs = PsTool()
+			} else {
+				currentCommand = append(currentCommand, strings.Split(pipeArgs, ` `)...)
+				pipeArgs = ExecTool(currentCommand[0], currentCommand[1:]...)
+				output = pipeArgs
+			}
+		}
+	} else {
+		command = RemovePlaceholders(command, replacements)
+		currentCommand := strings.Split(command, ` `)
+		if strings.Contains(currentCommand[0], `kill`) {
+			pid, _ := strconv.Atoi(currentCommand[1])
+			KillTool(pid)
+		} else if strings.Contains(currentCommand[0], `cd`) {
+			CdTool(currentCommand[1])
+		} else if strings.Contains(currentCommand[0], `pwd`) {
+			output = PwdTool()
+		} else if strings.Contains(currentCommand[0], `echo`) {
+			output = strings.Join(currentCommand[1:], ` `)
+		} else if strings.Contains(currentCommand[0], `ps`) {
+			output = PsTool()
+		} else if strings.Contains(currentCommand[0], `exit`) {
+			os.Exit(1)
+		} else {
+			output = ExecTool(currentCommand[0], currentCommand[1:]...)
+		}
+
+	}
+	return output
+}
+
+func CdTool(path string) {
+	if err := os.Chdir(path); err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func KillTool(pid int) {
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	if err := process.Kill(); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
+
+func PsTool() string {
+	processList, err := ps.Processes()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var builder strings.Builder
+	for x := range processList {
+		process := processList[x]
+		builder.WriteString(fmt.Sprintf("%d\t%s\n", process.Pid(), process.Executable()))
+	}
+	return builder.String()
+}
+
+func PwdTool() string {
+	curDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return curDir
+}
+
+func ExecTool(command string, inputArgs ...string) string {
+	cmd := exec.Command(command, inputArgs...)
+	cmdOut, _ := cmd.StdoutPipe()
+	cmdErr, _ := cmd.StderrPipe()
+	err := cmd.Start()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	bytesOut, _ := io.ReadAll(cmdOut)
+	bytesErr, _ := io.ReadAll(cmdErr)
+	if err := cmd.Wait(); err != nil {
+		fmt.Println(err.Error())
+	}
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	result, errorMsg := string(bytesOut), string(bytesErr)
+	if result == "" && errorMsg != "" {
+		fmt.Println(errorMsg)
+	}
+	cmdOut.Close()
+	cmdErr.Close()
+	return result
+}
+
 func main() {
 
+	currentUser, err := user.Current()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	host, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		curDir := PwdTool()
+		_, after, _ := strings.Cut(curDir, currentUser.HomeDir)
+		fmt.Printf("\n%s@%s:~%s$ ", currentUser.Name, host, after)
+		scanner.Scan()
+		result := Parse(scanner.Text())
+		if result != `` {
+			fmt.Print(result)
+		}
+	}
 }
--- task_l2/develop/go.mod
+++ task_l2/develop/go.mod
@@ -4,6 +4,7 @@ go 1.22.1
 
 require (
 	github.com/beevik/ntp v1.4.3 // indirect
+	github.com/mitchellh/go-ps v1.0.0
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
