4e480eb826f1e04c463791d2099de1bc1061b7cb
Leonov Vladislav <vladleonov21032002@gmail.com>
2024-06-20 20:20:46 +0300

    dev_06

--- task_l2/develop/dev06/task.go
+++ task_l2/develop/dev06/task.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -13,6 +23,74 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type Config struct {
+	Fields    []int
+	Delimeter string
+	Separated bool
+}
 
+func parseConfig() (*Config, error) {
+	fieldsFlag := flag.String("f", "", "fields (columns)")
+	delimiter := flag.String("d", "\t", "delimiter")
+	separated := flag.Bool("s", false, "delimeter only strings")
+	flag.Parse()
+
+	fields := strings.Split(*fieldsFlag, ",")
+	if len(fields) == 0 {
+		return nil, fmt.Errorf("invalid fields cnt")
+	}
+
+	conf := Config{}
+	for _, field := range fields {
+		f, err := strconv.Atoi(field)
+		if err != nil || f < 1 {
+			return nil, fmt.Errorf("invalid field")
+		}
+		conf.Fields = append(conf.Fields, f-1)
+	}
+	conf.Delimeter = *delimiter
+	conf.Separated = *separated
+	return &conf, nil
+}
+
+func cutLines(lines []string, conf *Config) []string {
+	var cutFields []string
+
+	for _, line := range lines {
+		if conf.Separated && !strings.Contains(line, conf.Delimeter) {
+			continue
+		}
+
+		fields := strings.Split(line, conf.Delimeter)
+		var currLine []string
+		for _, fieldId := range conf.Fields {
+			if fieldId < len(fields) {
+				currLine = append(currLine, fields[fieldId])
+			}
+		}
+		cutFields = append(cutFields, strings.Join(currLine, conf.Delimeter))
+	}
+	return cutFields
+}
+
+func main() {
+	conf, err := parseConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var lines []string
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		lines = append(lines, line)
+	}
+
+	separatedLines := cutLines(lines, conf)
+	for _, line := range separatedLines {
+		fmt.Println(line)
+	}
 }
--- task_l2/develop/dev06/task_test.go
+++ task_l2/develop/dev06/task_test.go
@@ -1,1 +1,134 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCutLines(t *testing.T) {
+	type args struct {
+		lines []string
+		conf  *Config
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "baseTest",
+			args: args{
+				lines: []string{"ssd"},
+				conf: &Config{
+					Fields:    []int{0},
+					Delimeter: "\t",
+					Separated: false,
+				},
+			},
+			want: []string{"ssd"},
+		},
+		{
+			name: "emptyInput",
+			args: args{
+				lines: nil,
+				conf: &Config{
+					Fields:    []int{1},
+					Delimeter: "\t",
+					Separated: false,
+				},
+			},
+			want: nil,
+		},
+		{
+			name: "singleField",
+			args: args{
+				lines: []string{"a\tb\tc"},
+				conf: &Config{
+					Fields:    []int{1},
+					Delimeter: "\t",
+					Separated: false,
+				},
+			},
+			want: []string{"b"},
+		},
+		{
+			name: "multipleFields",
+			args: args{
+				lines: []string{"a\tb\tc", "d\te\tf"},
+				conf: &Config{
+					Fields:    []int{0, 2},
+					Delimeter: "\t",
+					Separated: false,
+				},
+			},
+			want: []string{"a\tc", "d\tf"},
+		},
+		{
+			name: "differentDelimiter",
+			args: args{
+				lines: []string{"a,b,c", "d,e,f"},
+				conf: &Config{
+					Fields:    []int{1},
+					Delimeter: ",",
+					Separated: false,
+				},
+			},
+			want: []string{"b", "e"},
+		},
+		{
+			name: "separatedFieldTrue",
+			args: args{
+				lines: []string{"a b c", "d e f", "ghi"},
+				conf: &Config{
+					Fields:    []int{0, 1},
+					Delimeter: " ",
+					Separated: true,
+				},
+			},
+			want: []string{"a b", "d e"},
+		},
+		{
+			name: "separatedFieldFalse",
+			args: args{
+				lines: []string{"a b c", "d e f", "ghi"},
+				conf: &Config{
+					Fields:    []int{0, 1},
+					Delimeter: " ",
+					Separated: false,
+				},
+			},
+			want: []string{"a b", "d e", "ghi"},
+		},
+		{
+			name: "noDelimiterInSeparatedMode",
+			args: args{
+				lines: []string{"a b c", "def"},
+				conf: &Config{
+					Fields:    []int{0, 1},
+					Delimeter: " ",
+					Separated: true,
+				},
+			},
+			want: []string{"a b"},
+		},
+		{
+			name: "indexOutOfRange",
+			args: args{
+				lines: []string{"a\tb"},
+				conf: &Config{
+					Fields:    []int{2},
+					Delimeter: "\t",
+					Separated: false,
+				},
+			},
+			want: []string{""},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cutLines(tt.args.lines, tt.args.conf); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("cutLines() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
