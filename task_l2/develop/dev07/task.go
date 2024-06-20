d8676b6355ba1ed02b6ce683c4c624cbfe16beb3
Leonov Vladislav <vladleonov21032002@gmail.com>
2024-06-20 15:10:01 +0300

    dev_07

--- task_l2/develop/dev07/task.go
+++ task_l2/develop/dev07/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -30,9 +36,46 @@ start := time.Now()
 	sig(1*time.Minute),
 )
 
-fmt.Printf(“fone after %v”, time.Since(start))
+fmt.Printf(“done after %v”, time.Since(start))
 */
 
-func main() {
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	if len(channels) == 0 {
+		return nil
+	}
+	done := make(chan interface{})
+	var once sync.Once
 
+	for _, ch := range channels {
+		go func(ch <-chan interface{}) {
+			select {
+			case <-ch:
+				once.Do(func() { close(done) })
+			case <-done:
+			}
+		}(ch)
+	}
+	return done
+}
+
+func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("done after %v", time.Since(start))
 }
--- task_l2/develop/dev07/task_test.go
+++ task_l2/develop/dev07/task_test.go
@@ -1,1 +1,58 @@
 package main
+
+import (
+	"testing"
+	"time"
+)
+
+func createClosedChan() <-chan interface{} {
+	c := make(chan interface{})
+	close(c)
+	return c
+}
+
+func TestOrEmptyInput(t *testing.T) {
+	result := or()
+	if result != nil {
+		t.Errorf("Expected %v, got %v", nil, result)
+	}
+}
+
+func TestOrSingleInput(t *testing.T) {
+	c := createClosedChan()
+	result := or(c)
+	time.Sleep(time.Second * 1)
+
+	select {
+	case <-result:
+	default:
+		t.Errorf("chan wasn't closed")
+	}
+}
+func TestOrAllClosed(t *testing.T) {
+	c1 := createClosedChan()
+	c2 := createClosedChan()
+	c3 := createClosedChan()
+	result := or(c1, c2, c3)
+	time.Sleep(time.Second * 1)
+
+	select {
+	case <-result:
+	default:
+		t.Errorf("chan wasn't closed")
+	}
+}
+
+func TestOrOpenClose(t *testing.T) {
+	c1 := make(chan interface{})
+	c2 := createClosedChan()
+	c3 := make(chan interface{})
+	result := or(c1, c2, c3)
+	time.Sleep(time.Second * 1)
+
+	select {
+	case <-result:
+	default:
+		t.Errorf("chan wasn't closed")
+	}
+}
